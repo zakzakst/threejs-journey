5d3be1aa0fca3ac6b90531628b3cd3736a9d5ee1
zakzakst <e1433104@gmail.com>
2026-07-07 02:00:53 +0900

    fix

--- 09/src/script.js
+++ 09/src/script.js
@@ -1,6 +1,28 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import GUI from 'lil-gui'
+
+/**
+ * Debug
+ */
+const gui = new GUI(
+    {
+        width: 300,
+        title: 'Nice debug UI',
+        closeFolders: false
+    }
+)
+gui.close()
+gui.hide()
+
+window.addEventListener('keydown', (event) => {
+    if (event.key === 'h') {
+        gui.show(gui._hidden)
+    }
+})
+
+const debugObject = {}
 
 /**
  * Base
@@ -14,10 +36,58 @@ const scene = new THREE.Scene()
 /**
  * Object
  */
+debugObject.color = '#3a6ea6'
+
 const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
-const material = new THREE.MeshBasicMaterial({ color: '#ff0000' })
+const material = new THREE.MeshBasicMaterial({ color: debugObject.color, wireframe: true })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+const cubeTweaks = gui.addFolder('Awesome cube')
+cubeTweaks.close()
+
+// gui.add(mesh.position, 'y', -3, 3, 0.01)
+gui
+    .add(mesh.position, 'y')
+    .min(-3)
+    .max(3)
+    .step(0.01)
+    .name('elevation')
+
+gui
+    .add(mesh, 'visible')
+
+gui
+    .add(material, 'wireframe')
+
+gui
+    .addColor(debugObject, 'color')
+    .onChange((value) => {
+        // console.log(value.getHexString())
+        material.color.set(debugObject.color)
+    })
+
+debugObject.spin = () => {
+    gsap.to(mesh.rotation, {
+        y: mesh.rotation.y + Math.PI * 2
+    })
+}
+cubeTweaks
+    .add(debugObject, 'spin')
+
+debugObject.subdivision = 2
+cubeTweaks
+    .add(debugObject, 'subdivision')
+    .min(1)
+    .max(20)
+    .step(1)
+    .onFinishChange(() => {
+        mesh.geometry.dispose()
+        mesh.geometry = new THREE.BoxGeometry(
+            1, 1, 1,
+            debugObject.subdivision, debugObject.subdivision, debugObject.subdivision
+        )
+    })
 
 /**
  * Sizes
